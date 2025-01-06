496cdad7f5fd95cdf79a93021f00cc8ab9eacb99
JoseMDCloud <desarrollo01@mdcloud.mx>
2025-01-06 16:57:11 -0600

    Ca

--- Cliente/Ventas.php
+++ Cliente/Ventas.php
@@ -90,7 +90,7 @@ session_destroy(); */
 					</a>
 				</li>
 				<li>
-					<a href="Usuarios.php">
+					<a href="#">
 						<i class='bx bxs-user'></i>
 						<span class="text">Mis Clientes</span>
 					</a>
