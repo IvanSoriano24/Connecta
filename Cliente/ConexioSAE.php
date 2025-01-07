73ade3af608b6f2e481830a2488d132692c4bbd6
JoserMDCloud <desarrollo01@mdcloud.mx>
2025-01-07 16:00:49 -0600

    commit

--- Cliente/ConexioSAE.php
+++ Cliente/ConexioSAE.php
@@ -153,6 +153,7 @@ session_destroy(); */
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
   <script>
     $(document).ready(function () {
+      
       informaSae();
     });
   </script>
