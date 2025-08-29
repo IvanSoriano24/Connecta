6b8eef8dd6eeed28a81ba2326799b6ba37e977bd
JoseMDCloud <desarrollo01@mdcloud.mx>
2025-08-29 08:24:49 -0600

    cambios

--- Cliente/Parametros.php
+++ Cliente/Parametros.php
@@ -45,7 +45,7 @@ if (isset($_SESSION['usuario'])) {
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Boxicons -->
     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
-
+    <script src="JS/sideBar.js"></script>
     <!-- My CSS -->
     <link rel="stylesheet" href="CSS/style.css">
 
@@ -62,6 +62,7 @@ if (isset($_SESSION['usuario'])) {
         <?php include 'sidebar.php'; ?>
 
         <!-- CONTENT -->
+         <section id="content">
         <!-- Seccion de los Campos Libres -->
         <div id="invetarioFisico">
             <section id="content">
@@ -115,165 +116,15 @@ if (isset($_SESSION['usuario'])) {
                 <!-- MAIN -->
             </section>
         </div>
-        <!-- Modales -->
-        <!-- Modal Campos Libres-->
-        <div id="seleccionarCampoLibre" class="modal fade" tabindex="-1" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-lg">
-                <div class="modal-content">
-                    <!-- Encabezado -->
-                    <div class="modal-header bg-primary text-white border-0">
-                        <h5 class="modal-title w-100 text-center">Seleccionar Campo Libre</h5>
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Cerrar"></button>
-                    </div>
-                    <!-- Cuerpo -->
-                    <div class="modal-body px-4">
-                        <form id="formCamposLibres">
-                            <div class="row g-3">
-                                <!-- Seleccion de la tabla de SAE -->
-                                <div class="col-md-6">
-                                    <label for="selectTabla" class="form-label fw-semibold">Tabla</label>
-                                    <select id="selectTabla" class="form-select form-select-sm">
-                                        <option selected disabled>Seleccione una tabla</option>
-                                    </select>
-                                </div>
-                                <!-- Seleccion del campo de la tabla seleccionada -->
-                                <div class="col-md-6">
-                                    <label for="selectCampo" class="form-label fw-semibold">Campo</label>
-                                    <select id="selectCampo" class="form-select form-select-sm">
-                                        <option selected disabled>Seleccione un campo</option>
-                                    </select>
-                                </div>
-                                <!-- Campo de la descripcion del campo -->
-                                <div class="col-12">
-                                    <div class="form-floating">
-                                        <input type="text" class="form-control form-control-sm" id="descripcion" placeholder="Descripción">
-                                        <label for="descripcion">Descripción</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <hr class="my-4">
-                            <!-- Tabla con los campos usados de la tabla seleccionada -->
-                            <div id="camposSeleccionados">
-                                <h6 class="fw-semibold mb-3">Campos Seleccionados</h6>
-                                <ul id="listaCamposSeleccionado" class="list-group list-group-flush">
-                                    <!-- Los ítems seleccionados -->
-                                </ul>
-                            </div>
-                        </form>
-                    </div>
-                    <!-- Pie de modal con los botones-->
-                    <div class="modal-footer border-0">
-                        <!-- Boton para cancelar la accion y cerra el modal-->
-                        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancelar</button>
-                        <!-- Boton para guardar (revisar el archivo JS/parametros.js) para su funcionamiento -->
-                        <button type="button" class="btn btn-primary" id="btnGuardarCamposLibres">Guardar Campo Libre</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Modal Administrador-->
-        <div id="seleccionarClaveAdministrador" class="modal fade" tabindex="-1" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-lg">
-                <div class="modal-content">
-                    <!-- Encabezado -->
-                    <div class="modal-header bg-primary text-white border-0">
-                        <h5 class="modal-title w-100 text-center">Seleccionar Vendedor para Administrador</h5>
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Cerrar"></button>
-                    </div>
-                    <!-- Cuerpo -->
-                    <div class="modal-body">
-                        <form id="formCamposLibres" novalidate>
-                            <div class="row gx-3 gy-4">
-                                <!-- Nombre del administrador -->
-                                <div class="col-md-6">
-                                    <label for="nombreAdministrador" class="form-label fw-semibold">Administrador</label>
-                                    <input
-                                        type="text"
-                                        class="form-control form-control-sm"
-                                        id="nombreAdministrador"
-                                        name="nombreAdministrador"
-                                        readonly
-                                        tabindex="-1" />
-                                    <input type="hidden" id="idUsuario" name="idUsuario" />
-                                </div>
-                                <!-- Select de vendedores -->
-                                <div class="col-md-6">
-                                    <label for="vendedores" class="form-label fw-semibold">Vendedores</label>
-                                    <select
-                                        id="vendedores"
-                                        name="vendedores"
-                                        class="form-select form-select-sm"
-                                        required>
-                                        <option value="" disabled selected>Seleccione un Vendedor</option>
-                                        <!-- Opciones se inyectan vía JS -->
-                                    </select>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <!-- Pie de modal -->
-                    <div class="modal-footer border-0">
-                        <!-- Boton para cancelar y cerrar el modal -->
-                        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                            Cancelar
-                        </button>
-                        <!-- Boton para guardar (revisar el archivo JS/parametros.js) para su funcionamiento -->
-                        <button type="button" class="btn btn-primary" id="btnGuardarClave">
-                            Guardar
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Modal Series-->
-        <div id="seleccionarSerie" class="modal fade" tabindex="-1" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-lg">
-                <div class="modal-content">
-                    <!-- Encabezado -->
-                    <div class="modal-header bg-primary text-white border-0">
-                        <h5 class="modal-title w-100 text-center">Seleccionar Serie</h5>
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Cerrar"></button>
-                    </div>
-                    <!-- Cuerpo -->
-                    <div class="modal-body px-4">
-                        <form id="formCamposLibres">
-                            <div class="row g-3">
-                                <!-- Seleccion de folio -->
-                                <div class="col-md-6">
-                                    <label for="selectFolio" class="form-label fw-semibold">Documento</label>
-                                    <select id="selectFolio" class="form-select form-select-sm">
-                                        <option selected disabled>Seleccione un Tipo de Documento</option>
-                                    </select>
-                                </div>
-                                <!-- Seleccion del la serie -->
-                                <div class="col-md-6">
-                                    <label for="selectSerie" class="form-label fw-semibold">Serie</label>
-                                    <select id="selectSerie" class="form-select form-select-sm">
-                                        <option selected disabled>Seleccione la Serie</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <!-- Pie de modal con los botones-->
-                    <div class="modal-footer border-0">
-                        <!-- Boton para cancelar la accion y cerra el modal-->
-                        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancelar</button>
-                        <!-- Boton para guardar la seleccion (revisar el archivo JS/parametros.js) para su funcionamiento -->
-                        <button type="button" class="btn btn-primary" id="btnGuardarCamposLibres">Guardar Serie</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+        </section>
         <!-- CONTENT -->
     </div>
-    </section>
 
     <!-- JS Para el funcionamiento del sistema -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="JS/menu.js"></script>
     <script src="JS/app.js"></script>
+    <script src="JS/script.js"></script>
     <script src="JS/parametros.js"></script>
     <!-- Script para los botones de navegacion en la seccion de los campos libres -->
     <script>
